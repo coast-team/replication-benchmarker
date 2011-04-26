b70dfceb320bf315dc1fa1988851aaffe366e32c
mehdiahmednacer <mehdiahmednacer@47090b3f-bbb5-42d7-8af4-003170c53eba>
2011-04-26 20:33:51 +0000

    more test BoundaryStrategy
    git-svn-id: https://scm.gforge.inria.fr/svn/rtcelog/software/JBenchmarkerCE@381 47090b3f-bbb5-42d7-8af4-003170c53eba

--- test/jbenchmarker/logoot/TestBoundaryStrategy.java
+++ test/jbenchmarker/logoot/TestBoundaryStrategy.java
@@ -73,9 +73,9 @@ public class TestBoundaryStrategy {
         Q.addComponent(new Component(30, 6, 60));
         Q.addComponent(new Component(40, 6, 60));
 
-        ArrayList<LogootIdentifier> patch = BS.generateLineIdentifiers(LM, P, Q, 100);
+        ArrayList<LogootIdentifier> patch = BS.generateLineIdentifiers(LM, P, Q, 200);
 
-        assertEquals(100, patch.size());
+        assertEquals(200, patch.size());
 
         for (int i = 1; i < patch.size() - 1; i++) {
             assertTrue(patch.get(i).compareTo(P) > 0);
@@ -100,9 +100,9 @@ public class TestBoundaryStrategy {
         Q.addComponent(new Component(30, 4, 60));
         Q.addComponent(new Component(40, 4, 70));
 
-        ArrayList<LogootIdentifier> patch = BS.generateLineIdentifiers(LM, P, Q, 100);
+        ArrayList<LogootIdentifier> patch = BS.generateLineIdentifiers(LM, P, Q, 200);
 
-        assertEquals(100, patch.size());
+        assertEquals(200, patch.size());
 
         for (int i = 1; i < patch.size() - 1; i++) {
             assertTrue(patch.get(i).compareTo(P) > 0);
@@ -110,4 +110,126 @@ public class TestBoundaryStrategy {
             assertFalse(patch.get(i).compareTo(Q) > 0);
         }
     }
+    
+    
+    @Test
+    public void testgenerateLineIdentifiersCas4() {
+        LogootMerge LM = new LogootMerge(new LogootDocument(Long.MAX_VALUE), 1, 64, new BoundaryStrategy(1000000000));
+        BoundaryStrategy BS = new BoundaryStrategy(50);
+
+        LogootIdentifier P = new LogootIdentifier(3);
+        LogootIdentifier Q = new LogootIdentifier(3);
+
+
+        P.addComponent(new Component(31256, 4, 50));
+        P.addComponent(new Component(31256, 6, 60));
+        P.addComponent(new Component(31256, 6, 60));
+        
+        Q.addComponent(new Component(31256, 4, 50));
+        Q.addComponent(new Component(31256, 6, 60));
+        Q.addComponent(new Component(31257, 6, 60));
+        Q.addComponent(new Component(31256, 6, 60));
+
+        ArrayList<LogootIdentifier> patch = BS.generateLineIdentifiers(LM, P, Q, 200);
+
+        assertEquals(200, patch.size());
+
+        for (int i = 1; i < patch.size() - 1; i++) {
+            assertTrue(patch.get(i).compareTo(P) > 0);
+            assertTrue(patch.get(i).compareTo(patch.get(i - 1)) > 0);
+            assertFalse(patch.get(i).compareTo(Q) > 0);
+        }
+    }
+    
+    
+     @Test
+    public void testgenerateLineIdentifiersCas5() {
+        LogootMerge LM = new LogootMerge(new LogootDocument(Long.MAX_VALUE), 1, 64, new BoundaryStrategy(1000000000));
+        BoundaryStrategy BS = new BoundaryStrategy(50);
+
+        LogootIdentifier P = new LogootIdentifier(3);
+        LogootIdentifier Q = new LogootIdentifier(3);
+
+
+        P.addComponent(new Component(31256, 4, 50));
+        P.addComponent(new Component(31256, 6, 60));
+        P.addComponent(new Component(31256, 6, 60));
+        P.addComponent(new Component(31256, 6, 60));
+        
+        Q.addComponent(new Component(31256, 4, 50));
+        Q.addComponent(new Component(31256, 6, 60));
+        Q.addComponent(new Component(31256, 6, 60));
+        Q.addComponent(new Component(31256, 7, 60));
+
+        ArrayList<LogootIdentifier> patch = BS.generateLineIdentifiers(LM, P, Q, 200);
+
+        assertEquals(200, patch.size());
+
+        for (int i = 1; i < patch.size() - 1; i++) {
+            assertTrue(patch.get(i).compareTo(P) > 0);
+            assertTrue(patch.get(i).compareTo(patch.get(i - 1)) > 0);
+            assertFalse(patch.get(i).compareTo(Q) > 0);
+        }
+    }
+    
+     @Test
+    public void testgenerateLineIdentifiersCas6() {
+        LogootMerge LM = new LogootMerge(new LogootDocument(Long.MAX_VALUE), 1, 64, new BoundaryStrategy(1000000000));
+        BoundaryStrategy BS = new BoundaryStrategy(50);
+
+        LogootIdentifier P = new LogootIdentifier(3);
+        LogootIdentifier Q = new LogootIdentifier(3);
+
+
+        P.addComponent(new Component(31256, 4, 50));
+        P.addComponent(new Component(31256, 6, 60));
+        P.addComponent(new Component(31256, 6, 60));
+        P.addComponent(new Component(31255, 8, 60));
+        
+        Q.addComponent(new Component(31256, 4, 50));
+        Q.addComponent(new Component(31256, 6, 60));
+        Q.addComponent(new Component(31256, 6, 60));
+        Q.addComponent(new Component(31256, 7, 60));
+
+        ArrayList<LogootIdentifier> patch = BS.generateLineIdentifiers(LM, P, Q, 200);
+
+        assertEquals(200, patch.size());
+
+        for (int i = 1; i < patch.size() - 1; i++) {
+            assertTrue(patch.get(i).compareTo(P) > 0);
+            assertTrue(patch.get(i).compareTo(patch.get(i - 1)) > 0);
+            assertFalse(patch.get(i).compareTo(Q) > 0);
+        }
+    }
+     
+     @Test
+    public void testgenerateLineIdentifiersCas7() {
+        LogootMerge LM = new LogootMerge(new LogootDocument(Long.MAX_VALUE), 1, 64, new BoundaryStrategy(1000000000));
+        BoundaryStrategy BS = new BoundaryStrategy(50);
+
+        LogootIdentifier P = new LogootIdentifier(3);
+        LogootIdentifier Q = new LogootIdentifier(3);
+
+
+        P.addComponent(new Component(31256, 4, 50));
+        P.addComponent(new Component(31256, 6, 60));
+        P.addComponent(new Component(0, 6, 115));
+        P.addComponent(new Component(31256, 8, 60));
+        
+        Q.addComponent(new Component(31256, 4, 50));
+        Q.addComponent(new Component(31256, 6, 60));
+        Q.addComponent(new Component(0, 6, 115));
+        Q.addComponent(new Component(31256, 8, 61));
+
+        ArrayList<LogootIdentifier> patch = BS.generateLineIdentifiers(LM, P, Q, 200);
+
+        assertEquals(200, patch.size());
+
+        for (int i = 1; i < patch.size() - 1; i++) {
+            assertTrue(patch.get(i).compareTo(P) > 0);
+            assertTrue(patch.get(i).compareTo(patch.get(i - 1)) > 0);
+            assertFalse(patch.get(i).compareTo(Q) > 0);
+        }
+    }
+    
 }
