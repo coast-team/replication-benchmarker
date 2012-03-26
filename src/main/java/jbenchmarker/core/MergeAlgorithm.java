a175cf8af2f0057b1a38ce7ce0d2a0532ffbb84e
urso <urso@47090b3f-bbb5-42d7-8af4-003170c53eba>
2012-03-26 09:09:27 +0000

    remove debug overhead
    git-svn-id: https://scm.gforge.inria.fr/svn/rtcelog/software/JBenchmarkerCE@842 47090b3f-bbb5-42d7-8af4-003170c53eba

--- src/main/java/jbenchmarker/core/MergeAlgorithm.java
+++ src/main/java/jbenchmarker/core/MergeAlgorithm.java
@@ -31,37 +31,21 @@ import java.util.List;
  * @author urso
  */
 public abstract class MergeAlgorithm extends CRDT<String> implements Serializable{
+
+    private static final boolean DEBUG = false;
     
     // Supported Document
     final private Document doc;
-   
-    // Local operations generated
-    final private List<SequenceOperation> geneHistory; 
-
-    // All operations executed
-    final private List<SequenceMessage> history;
+       
+    final private List<String> states  = new ArrayList<String>(1);  
     
-    final private List<String> states;  
-    
-    
-    // Local operations generated time
-    final private List<Long> geneExecTime;
-    
-    // All operations executed  time 
-    final private List<Long> execTime;
     
     /*
      * Constructor
      */
     public MergeAlgorithm(Document doc, int r) {
-        this.execTime = new ArrayList<Long>();
-        this.geneExecTime = new ArrayList<Long>();
-        this.history = new ArrayList<SequenceMessage>();
-        this.geneHistory = new ArrayList<SequenceOperation>();
         this.setReplicaNumber(r);
         this.doc = doc;
-        
-        this.states = new ArrayList<String>();
     }
 
     /**
@@ -73,76 +57,12 @@ public abstract class MergeAlgorithm extends CRDT<String> implements Serializabl
      * To be define by the concrete merge algorithm
      */
     protected abstract List<SequenceMessage> generateLocal(SequenceOperation opt) throws IncorrectTraceException;
-    
-   
-    
-    /*
-     * Integration of a remote operation
-     * Adds operation in history and execution time
-     */
-    @Deprecated
-    public void integrate(SequenceMessage op) throws IncorrectTraceException {
-        history.add(op);
-        long startTime = System.nanoTime();
-        integrateLocal(op); 
-        execTime.add(System.nanoTime() - startTime);
         
-        states.add(doc.view());
-    }
-    
-    /**
-     *  Generation of a local trace operation, returns a patch of operation
-     * Throws IncorrectTraceException iff operation is not generable in the context.
-     **/    
-    @Deprecated
-    public List<SequenceMessage> generate(SequenceOperation opt) throws IncorrectTraceException {
-        geneHistory.add(opt);
-        long startTime = System.nanoTime();
-        List<SequenceMessage> l = generateLocal(opt);
-        long t = System.nanoTime() - startTime, oh = t/l.size();
-        int i = history.size();
-        geneExecTime.add(t);
-        for (SequenceMessage o : l) {
-            execTime.add(oh);
-            history.add(o);
-            i++;
-        }        
-        
-        states.add(doc.view());
-        
-        return l;
-    }
-
-    public List<Long> getExecTime() {
-        return execTime;
-    }
-
-    public List<SequenceMessage> getHistory() {
-        return history;
-    }
-
-    public List<Long> getLocalExecTime() {
-        return geneExecTime;
-    }
-
-    public List<SequenceOperation> getLocalHistory() {
-        return geneHistory;
-    }
-    
     public Document getDoc() {
         return doc;
     }
 
     public void reset() {
-        this.execTime.clear();
-        this.geneExecTime.clear();
-        this.getHistory().clear();
-        this.geneHistory.clear();       
-    }
-
-    @Override
-    public Long lastExecTime() {
-        return this.geneExecTime.get(this.geneExecTime.size()-1);
     }
 
     @Override
@@ -150,15 +70,13 @@ public abstract class MergeAlgorithm extends CRDT<String> implements Serializabl
         List<SequenceMessage> l = generateLocal((SequenceOperation) op);
         SequenceMessage m = null;
         for (SequenceMessage n : l) {
-            history.add(n);
             if (m == null) { 
                 m = n;
             } else {
                 m.concat(n);
             }
-        }
-        
-        states.add(doc.view());
+        }        
+        if (DEBUG) states.add(doc.view());
 
         return m;
     }
@@ -167,7 +85,6 @@ public abstract class MergeAlgorithm extends CRDT<String> implements Serializabl
     public void applyRemote(CRDTMessage msg) {
         try {
             SequenceMessage s = (SequenceMessage) msg;
-            history.add(s);
             integrateLocal(s);    
             for (Object m : s.getMsgs()) {
                 integrateLocal((SequenceMessage) m);
@@ -176,7 +93,7 @@ public abstract class MergeAlgorithm extends CRDT<String> implements Serializabl
             throw new IllegalStateException(ex);
         }
         
-        states.add(doc.view());
+        if (DEBUG) states.add(doc.view());
     }
 
     @Override
