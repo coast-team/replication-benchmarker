e1e0175dc84daf6d05a962ee6601b11a922e1a73
mehdiahmednacer <mehdiahmednacer@47090b3f-bbb5-42d7-8af4-003170c53eba>
2012-10-24 11:47:51 +0000

    Calcule time
    git-svn-id: https://scm.gforge.inria.fr/svn/rtcelog/software/JBenchmarkerCE@1260 47090b3f-bbb5-42d7-8af4-003170c53eba

--- src/main/java/jbenchmarker/trace/git/App.java
+++ src/main/java/jbenchmarker/trace/git/App.java
@@ -18,10 +18,16 @@
  */
 package jbenchmarker.trace.git;
 
+import crdt.CRDT;
+import crdt.Factory;
 import crdt.PreconditionException;
 import crdt.simulator.CausalSimulator;
 import crdt.simulator.IncorrectTraceException;
-import java.io.File;
+import crdt.simulator.Trace;
+import crdt.simulator.TraceFromFile;
+import java.io.BufferedWriter;
+import java.io.*;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.LinkedList;
@@ -38,27 +44,29 @@ import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.storage.file.FileRepositoryBuilder;
 import org.ektorp.DbAccessException;
 
-
 /**
  * Hello world!
  *
  */
 public class App {
 
+
     private final static String dbURL = "http://localhost:5984";
         
-    public static void main(String[] args) throws IOException, GitAPIException, IncorrectTraceException, PreconditionException {
+    public static void main(String[] args) throws IOException, GitAPIException, IncorrectTraceException, PreconditionException, ClassNotFoundException, InstantiationException, IllegalAccessException {
         //        GitExtraction.parseRepository(("/Users/urso/Rech/github/linux", "http://localhost:5984", "kernel/sched.c", true);
-        if (args.length < 1 ) {
+        if (args.length < 1) {
             System.err.println("Arguments : ");
             System.err.println("- git directory ");
             System.err.println("- file [optional] path or number (default : all files)");
             System.err.println("- --save [optional] save trace");
             System.err.println("- --clean [optional] clean DB");
+            System.err.println("- Number of execution");
+            System.err.println("- Factory");
             System.exit(1);
         }
         String gitdir = args[0];
-        
+
         List<String> paths = new LinkedList<String>();
         if (args.length > 1 && !args[1].startsWith("--") && !args[1].matches("[0-9]*")) {
             paths.add(args[1]);
@@ -66,45 +74,190 @@ public class App {
             extractFiles(new File(gitdir), gitdir, paths);
         }
         int end = paths.size();
-        if (args.length > 1 && args[1].matches("[0-9]*")) {
-            end = Integer.parseInt(args[1]);
-        }
-        
+//        if (args.length > 1 && args[1].matches("[0-9]*")) {
+//            end = Integer.parseInt(args[1]);
+//        }
+
         boolean save = Arrays.asList(args).contains("--save");
         boolean clean = Arrays.asList(args).contains("--clean");
         System.out.println("*** Total number of files : " + paths.size());
-        System.out.println("Path;Num;Replicas;Merges;Merge Blocks;Merge Size;Commits;Ins Blocks;Del Blocks;Upd Blocks;Ins Size;Del Size");
+        //System.out.println("Path;Num;Replicas;Merges;Merge Blocks;Merge Size;Commits;Ins Blocks;Del Blocks;Upd Blocks;Ins Size;Del Size");
+        String file = getNameFile(args);
+        writeTofile(file, "Path;Num;Replicas;Merges;Merge Blocks;Merge Size;Commits;Ins Blocks;Del Blocks;Upd Blocks;Ins Size;Del Size; Nbr Op; Local time; Remote Time; Memory");
+        
+        int nbrExec = Integer.parseInt(args[args.length - 2]);
+        Factory<CRDT> rf = (Factory<CRDT>) Class.forName(args[args.length - 1]).newInstance();
+
+        int nb = (nbrExec > 1) ? nbrExec + 1 : nbrExec;
+        boolean calculTimeEx = true;
+        
         int i = 0;
         CouchConnector cc = new CouchConnector(dbURL);
         for (String path : paths.subList(i, end)) {
-            for (int retry = 0; retry < 10; ++ retry) { // Overcome sporadic CouchDB Timeouts
+            for (int retry = 0; retry < 10; ++retry) { // Overcome sporadic CouchDB Timeouts
                 try {
+                    long ltime[][] = null, mem[][] = null, rtime[][] = null;
+                    int cop = 0, uop = 0, nbReplica = 0, mop = 0;
+                    int minCop = 0, minUop = 0, minMop = 0;
+                    String stat = "";
                     GitTrace trace = GitTrace.create(gitdir, cc, path, clean);
-                    CausalSimulator cd = new CausalSimulator(new LogootFactory());        
-                    cd.run(trace, false, save, 0, false);
-                    System.out.println(path + ';' + ++i + ';' + cd.replicas.keySet().size() + ';' 
-                            + trace.nbMerge + ';' + trace.nbBlockMerge + ';' + trace.mergeSize + ';' 
-                            + trace.nbCommit + ';'
-                            + trace.nbInsBlock + ';' + trace.nbDelBlock + ';' + trace.nbUpdBlock + ';'
-                            + trace.insertSize + ';' + trace.deleteSize );
+                    for (int k = 0; k < nbrExec; k++) {
+                        CausalSimulator cd = new CausalSimulator(rf);
+                        cd.run(trace, calculTimeEx, true, 100, true);
+                        if (k == 0) {
+                            cop = cd.splittedGenTime().size();
+                            uop = cd.replicaGenerationTimes().size();
+                            mop = cd.getMemUsed().size();
+                            nbReplica = cd.replicas.keySet().size();
+                            ltime = new long[nb][uop];
+                            rtime = new long[nb][cop];
+                            mem = new long[nb][mop];
+                            minCop = cop;
+                            minUop = uop;
+                            minMop = mop;
+                            
+                            stat = path + ';' + ++i + ';' + cd.replicas.keySet().size()
+                                + ';' + trace.nbMerge + ';' + trace.nbBlockMerge + 
+                                ';' + trace.mergeSize
+                                + ';' + trace.nbCommit + ';'
+                                + trace.nbInsBlock + ';' + trace.nbDelBlock
+                                + ';' + trace.nbUpdBlock + ';'
+                                + trace.insertSize + ';' + trace.deleteSize;
+                        }
+                        if (nbReplica == 0) {
+                            break;
+                        }
+
+                        if (minCop > cd.splittedGenTime().size()) {
+                            minCop = cd.splittedGenTime().size();
+                        }
+                        if (minUop > cd.replicaGenerationTimes().size()) {
+                            minUop = cd.replicaGenerationTimes().size();
+                        }
+
+                        if (calculTimeEx) {
+                            toArrayLong(ltime[k], cd.replicaGenerationTimes(), minUop);
+                            toArrayLong(rtime[k], cd.splittedGenTime(), minCop);
+                        }
+                        if (k == 0) {
+                            toArrayLong(mem[k], cd.getMemUsed(), minMop);
+                        }
+
+                        for (int j = 0; j < minCop - 1; j++) {
+                            if(nbReplica > 1)
+                            rtime[k][j] /= nbReplica - 1;
+                        }
+
+                        cd = null;
+                        //trace = null;
+                    }
+                    System.out.println(path + "; i : " + i);
+
+                    double thresold = 2.0;
+
+                    if (nbrExec > 1) {
+                        computeAverage(ltime, thresold, minUop);
+                        computeAverage(rtime, thresold, minCop);
+                    }
+
+                    long avgGen = calculAvg(ltime, minUop, "gen");
+                    long avgUsr = calculAvg(rtime, minCop, "usr");
+                    long avgMem = calculAvg(mem, minMop, "mem");
+                    stat = stat+ ';' +minCop + ';' + avgGen/1000 + ';' + avgUsr/1000 + ';' +avgMem;
+                    writeTofile(file, stat);
+                  
                     break;
-                } catch(DbAccessException e) {
+
+                } catch (DbAccessException e) {
                     if (retry == 9) {
                         throw e;
                     }
                 }
             }
-        }      
+        }
     }
 
     private static void extractFiles(File dir, String gitdir, List<String> paths) {
         for (File f : dir.listFiles()) {
             if (f.isFile()) { // && !f.getName().startsWith(".git")) { 
-                paths.add(f.getAbsolutePath().substring(gitdir.length()+1));
+                paths.add(f.getAbsolutePath().substring(gitdir.length() + 1));
             } else if (f.isDirectory() && !".git".equals(f.getName())) {
                 extractFiles(f, gitdir, paths);
             }
         }
+    }
+
+    private static void toArrayLong(long[] t, List<Long> l, int size) {
+        for (int i = 0; i < size; ++i) {
+            t[i] = l.get(i);
+        }
+    }
+
+    private static long calculAvg(long[][] data,int l, String type) throws IOException {
+
+        long avg = 0L;
+        if(type.equals("mem"))
+            for (int ex = 0; ex < l; ++ex)
+            avg += data[0][ex];
+        else
+            for (int ex = 0; ex < l; ++ex) {
+            avg += data[data.length - 1][ex];
+
+        }
+        
+        return avg;
+    }
+    
+    public static void writeTofile(String file, String s) throws IOException
+    {
+        FileWriter local = new FileWriter(file+".xlsx", true);
+
+        local.write(s+"\n");
+
+        if (local != null) {
+            local.close();
+        }
+    }
+
+    public static void computeAverage(long[][] data, double thresold, int l) {
+        int nbExpe = data.length - 1;
+
+        for (int op = 0; op < l; ++op) {
+            long sum = 0;
+            for (int ex = 0; ex < nbExpe; ++ex) {
+                sum += data[ex][op];
+            }
+            long moy = sum / nbExpe, sum2 = 0, k = 0;
+            for (int ex = 0; ex < nbExpe; ++ex) {
+                if (data[ex][op] < thresold * moy) {
+                    sum2 += data[ex][op];
+                    k++;
+                }
+            }
+            if (k != 0) {
+                data[nbExpe][op] = sum2 / k;
+            }
+        }
+    }
+    
+    public static String getNameFile(String args[]) {
+        int k = args[args.length - 1].lastIndexOf('.'), l = args[args.length - 1].lastIndexOf("Factory");
+        if (l == -1) {
+            l = args[0].lastIndexOf("Factories");
+        }
+
+        String n = args[args.length - 1].substring(k + 1, l);
+
+        String[] c;
+        if (n.contains("$")) {
+            c = n.split("\\$");
+            n = c[1];
+        }
+        if (n.equals("TTF")) {
+            String tab[] = args[0].split("\\$");
+            n = n + "" + tab[tab.length - 1];
+        }
+        return n;
     }
 }
 // git : 967 authors, 30000 commits: Total time: 5:46:52.821s 
