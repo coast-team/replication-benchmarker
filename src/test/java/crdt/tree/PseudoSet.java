c0d16c595eb9091a2b653ec3c97df94a6f7240d2
Stephane Martin <stephane@stephanemartin.fr>
2012-12-19 16:27:45 +0100

    Fix package mistake

--- src/test/java/crdt/tree/PseudoSet.java
+++ src/test/java/crdt/tree/PseudoSet.java
@@ -16,7 +16,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package crdttest.tree;
+package crdt.tree;
 
 import crdt.CRDTMessage;
 import crdt.PreconditionException;
--- src/test/java/crdt/tree/graphtree/ConnectPoliciesTest.java
+++ src/test/java/crdt/tree/graphtree/ConnectPoliciesTest.java
@@ -28,7 +28,7 @@ import crdt.tree.graphtree.connection.GraphSkip;
 import java.util.HashSet;
 import java.util.Iterator;
 import crdt.set.SetOperation;
-import crdttest.tree.PseudoSet;
+import crdt.tree.PseudoSet;
 import collect.Node;
 import java.util.Set;
 import collect.Tree;
