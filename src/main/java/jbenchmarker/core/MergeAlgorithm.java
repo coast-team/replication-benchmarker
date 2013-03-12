9c5c792fb6edd09aa4e52a30de245a5a552225b2
stephanemartin <stephanemartin@47090b3f-bbb5-42d7-8af4-003170c53eba>
2013-03-12 11:38:08 +0000

    +ApplyInsert for a delete operation (drinking is bad mkey - southpark ;) ) +Add  function in mergeAlgorithm to clear the code.
    git-svn-id: https://scm.gforge.inria.fr/svn/rtcelog/software/JBenchmarkerCE@1386 47090b3f-bbb5-42d7-8af4-003170c53eba

--- src/main/java/jbenchmarker/core/MergeAlgorithm.java
+++ src/main/java/jbenchmarker/core/MergeAlgorithm.java
@@ -1,20 +1,20 @@
 /**
  * Replication Benchmarker
- * https://github.com/score-team/replication-benchmarker/
- * Copyright (C) 2012 LORIA / Inria / SCORE Team
+ * https://github.com/score-team/replication-benchmarker/ Copyright (C) 2012
+ * LORIA / Inria / SCORE Team
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
  *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU General Public License along with
+ * this program. If not, see <http://www.gnu.org/licenses/>.
  */
 package jbenchmarker.core;
 
@@ -26,25 +26,24 @@ import java.util.List;
 
 /**
  * Squeleton of mergeAlgorithm
+ *
  * @author urso
  */
-public abstract class MergeAlgorithm extends CRDT<String> implements Serializable{
+public abstract class MergeAlgorithm extends CRDT<String> implements Serializable {
 
     private static final boolean DEBUG = false;
-    
     // Supported Document
     final private Document doc;
-       
-    final private List<String> states  = new ArrayList<String>(1);  
-    
-  /*  private void applyOneRemote(CommutativeMessage mess){
+    final private List<String> states = new ArrayList<String>(1);
+
+    /*  private void applyOneRemote(CommutativeMessage mess){
         
-    }*/
+     }*/
     /*
      * Constructor
      */
     /**
-     * 
+     *
      * @param doc Document of this merge algorithm
      * @param siteId SiteID or replicat number
      */
@@ -58,15 +57,16 @@ public abstract class MergeAlgorithm extends CRDT<String> implements Serializabl
     }
 
     /**
-     * Integrate remote message from another replicas
-     * To be define by the concrete merge algorithm
+     * Integrate remote message from another replicas To be define by the
+     * concrete merge algorithm
+     *
      * @param message from another replicas
-     * @throws IncorrectTraceException 
+     * @throws IncorrectTraceException
      */
     protected abstract void integrateRemote(SequenceMessage message) throws IncorrectTraceException;
-            
+
     /**
-     * 
+     *
      * @return the document
      */
     public Document getDoc() {
@@ -78,11 +78,32 @@ public abstract class MergeAlgorithm extends CRDT<String> implements Serializabl
      */
     public void reset() {
     }
+    /**
+     * Old school function need to be refactored in directly CRDT Message
+     * @param opt Sequence Operation
+     * @return List of message for sequence
+     * @throws IncorrectTraceException 
+     */
+    private List<SequenceMessage> oldApplyLocal(SequenceOperation opt) throws IncorrectTraceException {
+        switch (opt.getType()) {
+            case ins:
+                return localInsert(opt);
+            case del:
+                return localDelete(opt);
+            case update:
+                return localUpdate(opt);
+            case noop:
+                return null;
+            default:
+                throw new IncorrectTraceException("Unsupported operation : " + opt);
+        }
+    }
 
     /**
-     * Integreate local modification of the document.
-     * This function to stay compatible with causal dispatcher 
-     * localmodification is performed and a sequence message is transformed to CRDTMessage.
+     * Integreate local modification of the document. This function to stay
+     * compatible with causal dispatcher localmodification is performed and a
+     * sequence message is transformed to CRDTMessage.
+     *
      * @param op local operation
      * @return CRDT message will be sent to another
      * @throws PreconditionException
@@ -92,70 +113,61 @@ public abstract class MergeAlgorithm extends CRDT<String> implements Serializabl
         if (!(op instanceof SequenceOperation)) {
             throw new PreconditionException("Not a sequenceOperation : " + op);
         }
-        SequenceOperation opt = (SequenceOperation) op;
-        List<SequenceMessage> l;
-        switch (opt.getType()) {
-        case ins:
-            l = localInsert(opt);
-            break;
-        case del:
-            l =  localDelete(opt);
-            break;
-        case update:
-            l = localUpdate(opt);
-            break;
-        case noop:
+
+        List<SequenceMessage> l = oldApplyLocal((SequenceOperation) op);
+        if (l == null || l.isEmpty()) {
             return CRDTMessage.emptyMessage;
-        default:
-            throw new IncorrectTraceException("Unsupported operation : " + opt);
         }
-        
-        // FIXME: when l is an empty list, a NullPointerException is thrown...
         
         CRDTMessage m = null;
         for (SequenceMessage n : l) {
-            if (m == null) { 
+            if (m == null) {
                 m = new OperationBasedOneMessage(n);
             } else {
-                m=m.concat(new OperationBasedOneMessage(n));
+                m = m.concat(new OperationBasedOneMessage(n));
             }
-        }        
-        if (DEBUG) states.add(doc.view());
+        }
+        if (DEBUG) {
+            states.add(doc.view());
+        }
         return m;
     }
 
     final public CRDTMessage insert(int position, String content) throws PreconditionException {
-        return applyLocal(SequenceOperation.insert( position, content));
+        return applyLocal(SequenceOperation.insert(position, content));
     }
 
     final public CRDTMessage remove(int position, int offset) throws PreconditionException {
         return applyLocal(SequenceOperation.delete(position, offset));
     }
-    
+
     /**
      * integrate remote operations.
+     *
      * @param msg message generated by another replicas
      */
     /*@Override
-    public void applyRemote(CRDTMessage msg) {
-        ((CommutativeMessage)msg).execute(this);
+     public void applyRemote(CRDTMessage msg) {
+     ((CommutativeMessage)msg).execute(this);
         
-        if (DEBUG) states.add(doc.view());
-    }*/
+     if (DEBUG) states.add(doc.view());
+     }*/
     @Override
-    public void applyOneRemote(CRDTMessage mess){
-         try {            
-             integrateRemote(CRDTMessage2SequenceMessage(mess));
-         }catch (IncorrectTraceException ex) {
+    public void applyOneRemote(CRDTMessage mess) {
+        try {
+            integrateRemote(CRDTMessage2SequenceMessage(mess));
+        } catch (IncorrectTraceException ex) {
             throw new IllegalStateException(ex);
         }
     }
-    
-    public static SequenceMessage CRDTMessage2SequenceMessage(CRDTMessage mess){
+
+    public static SequenceMessage CRDTMessage2SequenceMessage(CRDTMessage mess) {
         return (SequenceMessage) ((OperationBasedOneMessage) mess).getOperation();
     }
+
     /**
-     * return document 
+     * return document
+     *
      * @return
      */
     @Override
@@ -164,16 +176,18 @@ public abstract class MergeAlgorithm extends CRDT<String> implements Serializabl
     }
 
     /**
-     * The defaut behavior of localInsert and localDelete is to call generateLocal.
-     * Either localXXXs or generateLocal should be overrriden.
+     * The defaut behavior of localInsert and localDelete is to call
+     * generateLocal. Either localXXXs or generateLocal should be overrriden.
+     *
      * @param opt local modification
-     * @return Messages generated by replication algorithm will sent to another replicas
+     * @return Messages generated by replication algorithm will sent to another
+     * replicas
      * @throws IncorrectTraceException by default
      */
     abstract protected List<SequenceMessage> localInsert(SequenceOperation opt) throws IncorrectTraceException;
 
     abstract protected List<SequenceMessage> localDelete(SequenceOperation opt) throws IncorrectTraceException;
-    
+
     /**
      * Default behavior of localUpdate is to throw IncorrectTraceException.
      */
